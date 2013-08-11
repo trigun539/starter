2d8c0b58839618ae8bf6f0e216d57bb298a13ec7
Edwin Perez <edwinperez539@gmail.com>
2013-08-11 17:20:59 -0400

    Added initial files to starter template

---
+++ client/grunt.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt){
+	grunt.initConfig({
+
+		// TASKS LIST
+
+	});
+
+	// Watch 
+	// watch: {
+	// 	sass: {
+	// 		files: ['app/sasss/*'], // Files to watch
+	// 		tasks: 'sass' // Task to execute whe files are changed
+	// 	}
+	// }
+
+	// Load NPM Tasks
+	// grunt.loadNpmTasks('grunt-contrib-sass');
+};
